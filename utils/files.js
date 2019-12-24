0b398888069763ea6b2c1d6de600a6e4d3ce294a
yangheng <bigyangmaster@163.com>
2019-12-24 14:11:17 +0800

    微信jssdk

--- utils/files.js
+++ utils/files.js
@@ -8,40 +8,27 @@ const sha1 = (str) => {
     return str;
 };
 
-exports.readFileAsync = function (path, encoding) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, encoding, (err, data) => {
-            if (err) reject(err);
-            else {
-                // return  string
-                resolve(data)
-            }
-        })
-    })
-};
-
-exports.writeFileAsync = function (path, content) {
-    return new Promise((resolve, reject) => {
-        let str;
-        if (typeof content !== "string") str = JSON.stringify(content);
-        fs.writeFile(path, str, (err) => {
-            if (err) reject(err);
-
-            // return origin(content)
-            else resolve(content);
-        })
-    })
-};
-
-
+/**
+ * 生成当前时间戳
+ * @returns {string}
+ */
 function createTimestamp () {
     return parseInt(new Date().getTime() / 1000) + '';
 }
+
+/**
+ * 生成nonceStr
+ * @returns {string}
+ */
 function createNonceStr () {
     return Math.random().toString(36).substr(2, 15);
 }
 
-// 将object 转换成 query 的格式
+/**
+ * 转换query
+ * @param ob
+ * @returns {string}
+ */
 function stringPathQuery(ob) {
     if (!ob) return '';
     let queryArr = [];
@@ -51,8 +38,13 @@ function stringPathQuery(ob) {
     return queryArr.join('&');
 }
 
-//
-exports.getSignPackage= function  (ticket, url) {
+/**
+ * 获取签名数据
+ * @param ticket
+ * @param url
+ * @returns {*}
+ */
+function getSignPackage (ticket, url) {
     if (!ticket || !url) return {
         error: 'url没传或ticket票据没获到'
     };
@@ -63,8 +55,50 @@ exports.getSignPackage= function  (ticket, url) {
         url: url
     };
     const queryStr = stringPathQuery(ob);
-    console.log(queryStr);
     return Object.assign(ob, {
         signature: sha1(queryStr)
     });
+}
+
+/**
+ * 读取文件数据
+ * @param path
+ * @param encoding
+ * @returns {Promise<any>}
+ */
+function readFileAsync (path, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, encoding, (err, data) => {
+            if (err) reject(err);
+            else {
+                // return  string
+                resolve(data)
+            }
+        })
+    })
+}
+
+/**
+ * 写数据到文件中
+ * @param path
+ * @param content
+ * @returns {Promise<any>}
+ */
+function writeFileAsync (path, content) {
+    return new Promise((resolve, reject) => {
+        let str;
+        if (typeof content !== "string") str = JSON.stringify(content);
+        fs.writeFile(path, str, (err) => {
+            if (err) reject(err);
+
+            // return origin(content)
+            else resolve(content);
+        })
+    })
+}
+
+module.exports = {
+    readFileAsync,
+    writeFileAsync,
+    getSignPackage,
 };
